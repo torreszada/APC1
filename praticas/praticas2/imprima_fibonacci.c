b5c1c5fc174e0514c485b20ea9c2c9f9d8736caf
theo <torrestheo76@gmail.com>
2025-03-19 10:54:42 -0300

    Adicionando arquivo da praticas2

---
+++ praticas/praticas2/imprima_fibonacci.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("%i %i 1 1\n");
+    printf("      1 2 1\n");
+    printf("    1 2 3 2 1\n");
+    printf(" 1 2 3 5 3 2 1\n");
+    printf("1 2 3 5 8 5 3 2 1\n");
+    return 0;
+}
---
+++ praticas/praticas2/imprima_seu_nome.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Theo Torres Rodrigues Batista\n");
+  return 0;
+}
